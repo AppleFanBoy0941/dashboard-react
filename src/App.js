abc6db61fcd69f563b2c335b839fab81a8abd6d3
Mark Brodersen <U2405963@rts-365.dk>
2022-08-25 14:07:15 +0200

    Mobile version done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import useLocalStorage from "./hooks/useLocalStorage";
+import Nav from "./templates/Nav";
 
 function App() {
   const [count, setCount] = useLocalStorage("count", 0);
   return (
     <div className="App">
+      <Nav />
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>+</button>
     </div>
