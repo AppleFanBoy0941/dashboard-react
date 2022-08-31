cdabdb2b039c8f37e554fc2e269c888f91c5125c
Mark Brodersen <U2405963@rts-365.dk>
2022-08-31 10:50:13 +0200

    Date inputs

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,8 +1,8 @@
 // import { Doughnut } from "react-chartjs-2";
-import useFetch from "../hooks/useFetch";
+// import useFetch from "../hooks/useFetch";
 
 const Charts = () => {
-  const { data } = useFetch("http://localhost:3001/orders");
+  // const { data } = useFetch("http://localhost:3001/orders");
   // let viewsForDay =
   //   data[0].total + data[1].total + data[2].total + data[3].total;
   // console.log(viewsForDay);
--- src/pages/Bestillinger.js
+++ src/pages/Bestillinger.js
@@ -1,8 +1,27 @@
 import Charts from "../components/Charts";
-import Search from "../components/Search";
 import Input from "../components/subcomponents/Input";
 import Wrapper from "../templates/Wrapper";
+import useFetch from "../hooks/useFetch";
+
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 const Bestillinger = () => {
+  const d = new Date();
+  const date = d.toLocaleDateString();
+  const pastDate = d.setDate(d.getDate() - 30);
+  const past = new Date(pastDate).toLocaleDateString();
+  const [fromDate, setFromDate] = useState(past);
+  const [toDate, setToDate] = useState(date);
+  // console.log(`date: ${date} past:${past}`);
+
+  const [collapsed, setCollapsed] = useState(true);
+  const [customerId, setCustomerId] = useState("");
+
+  const { data: users } = useFetch("http://localhost:3001/users");
+  const { data } = useFetch("http://localhost:3001/orders");
+  console.log(data);
+  console.log(users);
+
   return (
     <section className="flex flex-col lg:flex-row pt-24">
       <div>
@@ -15,32 +34,74 @@ const Bestillinger = () => {
           </div>
           <div className="px-4">
             <Wrapper>
-              <h3>Bestillinger</h3>
-              <div>
-                <h3>Filter</h3>
+              <h3 className="text-slate-600 font-bold text-2xl">
+                Bestillinger
+              </h3>
+              <div className="pt-4">
+                <h3 className="text-slate-500 font-semibold text-xl pb-2">
+                  Filter
+                </h3>
                 <div className="flex">
-                  <search></search>
+                  {/* <search></search> */}
                   <button></button>
                 </div>
                 <div className="flex">
                   <Input
                     type="date"
-                    label=""
-                    value
-                    setValue=""
-                    error
+                    label="Fra dato"
+                    value={fromDate}
+                    setValue={setFromDate}
                     id="from"
                   />
-                  <Input type="date" label="" value setValue error id="to" />
+                  <Input
+                    type="date"
+                    label="Til dato"
+                    value={toDate}
+                    setValue={setToDate}
+                    id="to"
+                  />
                 </div>
               </div>
-              <div></div>
+              <div>
+                {data &&
+                  data.map(function (order) {
+                    return (
+                      <div
+                        key={order.id}
+                        onClick={() => setCustomerId(order.id)}
+                        className="flex justify-between py-2"
+                      >
+                        <div>
+                          <h2 className="text-slate-800 font-bold">
+                            {order.id}
+                          </h2>
+                          <p className="text-slate-400 font-semibold">
+                            {order.date}
+                          </p>
+                          <span className="text-slate-400">{order.time}</span>
+                        </div>
+                        <div className="flex flex-col items-end">
+                          <button className="justify-self-end bg-slate-200 w-8 h-8 rounded-full"></button>
+                          <div className="flex">
+                            <p>#{order.customer_id}</p>
+                            <img src={users.image} alt={users.name} />
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+              </div>
             </Wrapper>
           </div>
         </div>
-        <div className="bg-slate-100"></div>
       </div>
-      <div></div>
+      <AnimatePresence>
+        {collapsed && (
+          <motion.div className="bg-slate-800 w-screen fixed bottom-0 h-32 rounded-t-md">
+            <h2 className="text-white">Ordre {customerId}</h2>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </section>
   );
 };
