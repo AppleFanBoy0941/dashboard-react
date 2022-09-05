eaaab14e66c6207f205cf2609a3aacbba160683d
Mark Brodersen <U2405963@rts-365.dk>
2022-09-05 08:30:44 +0200

    IT LIVESSS!!! AND WORKS!!!!

--- src/components/UserOrders.js
+++ src/components/UserOrders.js
@@ -2,27 +2,24 @@ import { useState } from "react";
 import FeatherIcons from "feather-icons-react";
 import useFetch from "../hooks/useFetch";
 
-const UserOrders = ({ id, customerId }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const UserOrders = ({ id, isOpen, setIsOpen }) => {
   const { data, isLoading, error } = useFetch(
     `http://localhost:3001/orders?id=${id}`
   );
-  console.log(data);
   const { data: customerData } = useFetch(
     `http://localhost:3001/users/${customerId}`
   );
   let item = data[0];
-  console.log(item);
 
   return (
     <div className="py-5 px-5 ">
       <div className="flex justify-between">
-        <span className="text-slate-400">Ordre: {item.id}</span>
-        <button>
+        <span className="text-slate-400">Ordre: {item?.id}</span>
+        <button onClick={() => setIsOpen(!isOpen)}>
           <FeatherIcons icon="chevron-up" />
         </button>
       </div>
-      <h2 className="text-white text-2xl">{item.date}</h2>
+      <h2 className="text-white text-2xl">{item?.date}</h2>
     </div>
   );
 };
